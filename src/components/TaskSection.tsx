8748525677ffea408a9973ad698328f9ca1f4eff
vitor0ferreira <vitor_hugo.f.s@hotmail.com>
2023-08-20 21:52:53 -0300

    Loading Spinner added

--- src/components/TaskSection.tsx
+++ src/components/TaskSection.tsx
@@ -2,6 +2,7 @@
 import { UseHandleTasks } from "@/hooks/UseHandleTasks"
 import Task from "./Task/Task"
 import { useEffect, useState } from "react"
+import LoadingCircle from "./LoadingCircle"
 
 export default function TaskSection() {
 
@@ -13,10 +14,10 @@ export default function TaskSection() {
   },[taskList])
 
   return (
-    <section id='taskList' className='flex flex-col bg-indigo-100 shadow-lg overflow-hidden min-h-[10rem] gap-1 h-max w-9/12 m-2'>
-      {tasks && tasks.map((task:any, index:number)=>(
+    <section id='taskList' className='flex flex-col relative bg-indigo-100 shadow-lg overflow-hidden min-h-[10rem] gap-1 h-max w-9/12 m-2'>
+      {tasks ? tasks.map((task:any, index:number)=>(
           <Task task={task} key={index}/>
-      ))}
+      )) : <LoadingCircle/>}
     </section>
   )
 }
