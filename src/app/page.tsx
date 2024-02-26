0869abc6c67abf0aa5c5a3cc5518596718e10477
vitor0ferreira <vitor_hugo.f.s@hotmail.com>
2024-02-26 20:50:58 -0300

    feat: make form work with validation, add UI tasks adjust

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
 'use client'
 
-import LoadingCircle from "@/components/LoadingCircle";
 import TaskCard from "@/components/Task/TaskCard";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 interface Task {
   id: string,
   title: string,
   date: string,
-  createdAt: Date,
+  createdAt: string,
   priority: string
 }
 
@@ -21,6 +20,14 @@ export default function Home() {
     }
     return null
   })
+  const [titleInput, setTitleInput] = useState<string>('')
+  const [dateInput, setDateInput] = useState<string>()
+  const [radioInput, setRadioInput] = useState<string | number>(0)
+  const priorityString: any = {
+    0: 'low',
+    50: 'medium',
+    100: 'high'
+  }
 
   function createTask([title, date, priority]:string[]) {
 
@@ -28,11 +35,15 @@ export default function Home() {
       id: crypto.randomUUID(),
       title,
       date,
-      createdAt: new Date(),
+      createdAt: new Date().toLocaleDateString(),
       priority
     }
     const taskArray = tasks ? [newTask, ...tasks] : [newTask]
     setTasks(taskArray)
+    console.log(titleInput, dateInput, priorityString[radioInput])
+    setDateInput('')
+    setRadioInput(0)
+    setTitleInput('')
   }
 
   function removeTask(id: string) {
@@ -42,34 +53,33 @@ export default function Home() {
     setTasks(taskArray)
   }
 
-  const novaTask = ['titulo', 'data', 'high']
 
   if (tasks !== null) {
     localStorage.setItem("tasks", JSON.stringify(tasks));
   }
 
-  const [radioInput, setRadioInput] = useState(0)
+  const newTask = [titleInput, dateInput, priorityString[radioInput]]
 
   return (
-    <main className="flex flex-col items-center justify-start pt-8 gap-8 min-h-screen min-w-screen">
+    <main className="flex flex-col items-center justify-start p-8 gap-8 min-h-screen w-full">
       <h1 className="block font-extrabold text-4xl">TO DO MANAGER</h1>
 
       {/* New Task Form */}
-      <form action="" className="flex justify-evenly items-center gap-10 py-4 px-8 w-max rounded-md bg-slate-600">
+      <form className="flex flex-wrap justify-evenly items-center gap-10 py-4 px-8 w-max max-w-[80%] rounded-md bg-slate-600">
 
         <div className="flex flex-col gap-1 font-semibold">
           <label htmlFor="titleInput" className="text-slate-100">Title</label>
-          <input id="titleInput" type="text" className="bg-slate-100 focus-visible:outline-none p-2 h-6"/>
+          <input id="titleInput" type="text" required value={titleInput} onChange={(e)=>setTitleInput(e.target.value)} className="bg-slate-100 focus-visible:outline-none p-2 h-6"/>
         </div>
 
         <div className="flex flex-col gap-1 font-semibold">
           <label htmlFor="dateInput" className="text-slate-100">Date</label>
-          <input type="date" id="dateInput"  className="bg-slate-100 text-slate-900 focus-visible:outline-none p-2 h-6"/>
+          <input type="date" id="dateInput" required value={dateInput} onChange={(e)=>setDateInput(e.target.value)}  className="bg-slate-100 text-slate-900 focus-visible:outline-none p-2 h-6"/>
         </div>
 
         <div className="flex flex-col gap-1">
           <label htmlFor="priorityInput" className="font-semibold h-min text-slate-100">Priority</label>
-          <input type="range" id="priorityInput" min={0} max={100} step={50} list="radioMarkers" className="bg-slate-100 appearance-none cursor-pointer h-2 flex items-center rounded-full [&::-webkit-slider-runnable-track]:rounded-full [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:h-2 [&::-webkit-slider-thumb]:w-4 [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-red-500"/>
+          <input type="range" id="priorityInput" required min={0} max={100} step={50} value={radioInput} onChange={(e)=>setRadioInput(e.target.value)} list="radioMarkers" className="bg-slate-100 appearance-none cursor-pointer h-2 flex items-center rounded-full [&::-webkit-slider-runnable-track]:rounded-full [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:h-2 [&::-webkit-slider-thumb]:w-4 [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-red-500"/>
           <datalist id="radioMarkers" className="flex justify-between text-sm text-slate-100">
             <option value="0" label="Low"></option>
             <option value="50" label="Medium"></option>
@@ -81,14 +91,19 @@ export default function Home() {
 
       <button
         type="button"
-        onClick={() => createTask(novaTask)}
+        onClick={() => {
+          if(newTask[0] != '' && newTask[1] != '' && newTask[2] != undefined)
+            createTask(newTask)
+          else
+            alert('Fill all the camps to create a new task')
+        }}
         className="p-2 rounded-md font-bold bg-slate-700 text-white"
       >
-        Criar nova task
+        Create a new task
       </button>
       <section
         id="taskSection"
-        className="flex flex-col justify-start relative p-4 bg-indigo-100 overflow-hidden gap-2 h-max min-w-min w-max m-2"
+        className="flex flex-col justify-start relative p-4 bg-indigo-100 overflow-hidden gap-2 h-max w-max max-w-[80%] m-2"
       >
         {tasks[0] != null ? (
           tasks.map((task) => {
@@ -103,7 +118,7 @@ export default function Home() {
       <button
         type="button"
         onClick={() => setTasks([])}
-        className="bg-red-500 text-md rounded-md p-2 font-bold text-white hover:bg-red-600 shadow-md"
+        className="bg-red-500 text-md rounded-md p-2 mb-10 font-bold text-white hover:bg-red-600 shadow-md"
       >
         Remove All Tasks
       </button>
--- src/components/Task/TaskCard.tsx
+++ src/components/Task/TaskCard.tsx
@@ -12,28 +12,25 @@ export default function TaskCard({props, task, removeTask}:any) {
 
   switch (task.priority){
     case 'high':
-      priorityColor.text = 'text-red-600'
+      priorityColor.text = 'text-slate-50'
       priorityColor.bg = 'bg-red-600'
       break
     case 'medium':
-      priorityColor.text = 'text-amber-500'
-      priorityColor.bg = 'bg-amber-500'
+      priorityColor.text = 'text-slate-950'
+      priorityColor.bg = 'bg-amber-400'
       break
     case 'low':
-      priorityColor.text = 'text-green-600'
-      priorityColor.bg = 'bg-green-600'
+      priorityColor.text = 'text-slate-50'
+      priorityColor.bg = 'bg-green-700'
       break
   }
   
   return (
-    <article className="border-white grid gap-4 grid-cols-5 border-b-4">
-      <div className={`${priorityColor.bg} w-6 aspect-square`} />
-      <span className="font-bold ">{task.title}</span>
+    <article className={`border-white flex items-center gap-4 p-2 rounded-md min-w-[500px] w-max ${priorityColor.bg} ${priorityColor.text}`}>
+      <span className='font-bold'>{task.createdAt}</span>
+      <span className="font-bold flex-1">{task.title}</span>
       <span className="font-bold ">{task.date}</span>
-      <span className={`${priorityColor.text} font-bold drop-shadow-lg`}>
-        Prioridade
-      </span>
-      <button className="text-red-500 bg-slate-400 p-2" onClick={()=>removeTask(task.id)}>
+      <button className="text-red-500 p-2 hover:text-amber-500 bg-slate-200 rounded-full" onClick={()=>removeTask(task.id)}>
         <FaDeleteLeft />
       </button>
     </article>
